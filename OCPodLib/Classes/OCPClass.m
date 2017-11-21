7051ebdb6c71b6ae876dd8bcd2de9f1d168b271d
Steve <chengquan.wang@ele.me>
2017-11-21 21:43:32 +0800

    init

---
+++ OCPodLib/Classes/OCPClass.m
@@ -0,0 +1,17 @@
+//
+//  OCPClass.m
+//  OCPodLib
+//
+//  Created by Steve on 21/11/2017.
+//
+
+#import "OCPClass.h"
+
+@implementation OCPClass
+
+- (instancetype)init {
+    NSLog(@"I am here");
+    return  [super init];
+}
+
+@end
